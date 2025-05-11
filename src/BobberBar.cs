4fb2def8c82e141d1b53f952110970721a3680f0
s53kr-lm8a6r <iam.msams@gmail.com>
2025-05-11 15:12:51 +0700

    added a perfect chance based on fishes difficulties

--- src/BobberBar.cs
+++ src/BobberBar.cs
@@ -48,8 +48,37 @@ internal sealed partial class ModEntry
         // Catch treasure
         bobberBar.treasureCaught = bobberBar.treasure && _config.SkipMinigameWithTreasure;
 
-        // Perfect on demand, since it is perfect with default so do not need to check like treasure
-        bobberBar.perfect = _config.SkipMinigameWithPerfect;
+        if (_config.SkipMinigameWithPerfect)
+            // Perfect on demand, since it is perfect with default so do not need to check like treasure
+            bobberBar.perfect = true;
+        else
+        {
+            int baseChance; // in percentage
+            switch (bobberBar.motionType)
+            {
+                // Dart
+                case 1:
+                    baseChance = 5;
+                    break;
+                // Smooth
+                case 2:
+                    baseChance = 90;
+                    break;
+                // Floater & Sinker
+                case 3:
+                case 4:
+                    baseChance = 22;
+                    break;
+                // Mixed
+                default:
+                    baseChance = 54;
+                    break;
+            }
+            baseChance = bobberBar.bossFish ? (int)Math.Ceiling(baseChance / 5f) : baseChance;
+            double difficultyMultiplier = (-3.72f + (123f / (1 + Math.Pow(bobberBar.difficulty / 44.29f, 2.11f)))) / 100;
+
+            bobberBar.perfect = new Random().Next(0, 100) <= (baseChance * difficultyMultiplier);
+        }
 
         msg.message = Helper.Translation.Get("bobber-bar.familiar");
         Game1.addHUDMessage(msg);
